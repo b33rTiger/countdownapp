d287f353b20366876cc0b873b0bdbbe34208b739
John <john@johnsheets.me>
2017-07-06 12:54:38 -0700

    setstate functioning

--- src/app.jsx
+++ src/app.jsx
@@ -5,12 +5,13 @@ class App extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      deadline: 'December 25, 2017'
+      deadline: 'December 25, 2017',
+      newDeadline: ''
     }
   }
 
   changeDeadline() {
-    this.setState({deadline: 'November'})
+    this.setState({deadline: this.state.newDeadline});
   }
   render() {
     return (
@@ -24,7 +25,7 @@ class App extends Component {
           <div className="clock-secs">20 Seconds</div>
         </div>
         <div>
-          <input placeholder="new date" />
+          <input onChange={event => this.setState({newDeadline: event.target.value})} placeholder="new date" />
           <button onClick={() => this.changeDeadline()}>
           Submit
           </button>
