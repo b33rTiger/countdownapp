7c545c84c6ee4c425c502d3c40317f93a7913931
John <john@johnsheets.me>
2017-07-06 12:37:02 -0700

    app restructure

---
+++ src/app.jsx
@@ -0,0 +1,31 @@
+import React, { Component } from 'react';
+import './app.css';
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      deadline: 'December 25, 2017'
+    }
+  }
+  render() {
+    return (
+      <div className="app">
+        <div className="app-title">
+        Countdown to {this.state.deadline}</div>
+        <div>
+          <div className="clock-days">14 Days</div>
+          <div className="clock-hrs">30 Hours</div>
+          <div className="clock-mins">15 Minutes</div>
+          <div className="clock-secs">20 Seconds</div>
+        </div>
+        <div>
+          <input placeholder="new date" />
+          <button>Submit</button>
+        </div>
+      </div>
+    )
+  }
+}
+
+export default App;
